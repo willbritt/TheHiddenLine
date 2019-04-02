449004cfa1eac2fbb1844ac86cae241929b7d8db
willbritt <will.britt@centre.edu>
2019-04-02 14:49:30 -0400

    setup

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+var dataP = d3.json("gradeData.json");
 
+dataP.then(function(data)
+{
+  console.log("data",data);
+  drawChart(data, "#Line");
+  drawChart(data, "Area")
+},
+function(err)
+{
+  console.log(err);
+});
+
+var drawChart = function(data, idname)
+{
