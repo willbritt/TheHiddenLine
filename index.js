c66d056abf50082b101916f6b7fdb2ef0bab9974
willbritt <will.britt@centre.edu>
2019-04-09 00:03:19 -0400

    done

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var dataP = d3.json("classData.json");
 
 dataP.then(function(data)
 {
-    console.log(data);
     drawLineChart(data, "#chart0", 0, screenSettings, marginSettings,"Bookworm Penguin");
+    //drawLineChart2(data, "#chart23", 0, screenSettings, marginSettings,"Bookworm Penguin");
     drawLineChart(data, "#chart1", 1, screenSettings, marginSettings,"Crafty Penguin");
     drawLineChart(data, "#chart2", 2, screenSettings, marginSettings,"Cyclist Penguin");
     drawLineChart(data, "#chart3", 3, screenSettings, marginSettings,"Drunken Penguin");
@@ -43,13 +43,112 @@ var marginSettings = {
 }
 
 //// Line Chart  ////
+var drawLineChart2 = function(dataSet, svgSelector, index, screen, margins, name)
+{
+  console.log("start")
+  //
+  var HwData = dataSet[index].homework
+  //console.log("HwData", HwData)
+  //console.log(HwData.length)
+  var graphWidth  = screen.width - margins.left - margins.right;
+  var graphHeight = screen.height - margins.top - margins.bottom;
+  var borderWidth = 1;
+
+  var xScale = d3.scaleLinear()
+                .domain([1, HwData.length])
+                .range([margins.left,(graphWidth-margins.right)]);
+  var xAxis = d3.axisBottom()
+                .scale(xScale)
+                .tickValues([5,10,15,20,25,30,35,40]);
+
+  var yScale = d3.scaleLinear()
+                .domain([0, 10])
+                .range([0, graphHeight - 70 ]);
+
+  var yAxisScale = d3.scaleLinear()
+                .domain([0, 100])
+                .range([graphHeight, 0]);
+
+  var yAxis = d3.axisLeft()
+                .scale(yAxisScale);
+
+
+  var colorScale = d3.scaleOrdinal(d3.schemeAccent);
+
+  var graphSVG = d3.select(svgSelector)
+              .attr("width", screen.width)
+              .attr("height", screen.height);
+
+
+  graphBorder = graphSVG.append("rect")
+                     .attr("border-style", "solid")
+                     .attr("x", margins.left)
+                     .attr("y", margins.top)
+                     .attr("width", graphWidth)
+                     .attr("height", graphHeight)
+                     .attr("fill", "#d5f4e6")
+                     .style("stroke", "black")
+                     .style("stroke-width", borderWidth)
+                     .classed("graph-border", true);
+
+
+  var graphData = graphSVG.append("g")
+                    .attr("x", margins.left)
+                    .attr("y", margins.top)
+                    .classed("graph-data", true);
+
+var line = d3.line()
+          .x(function(d){return xScale(d.day)})
+          .y(function(d){return yScale(d.grade)});
+
+ var lineDrawer = graphSVG.append("path")
+                          .datum(HwData)
+                          .attr("fill", colorScale("quiz"))
+                          .attr("fill", "none")
+                          .attr("stroke", "steelblue")
+                          .attr("stroke-linejoin", "round")
+                          .attr("stroke-linecap", "round")
+                          .attr("stroke-width", 3)
+                          .attr("class","line")
+                          .attr("d",line);
+
+ var xAxisGraphic = graphSVG.append('g')
+                    .call(xAxis)
+                    .attr("transform","translate("+0+"," +(graphHeight + 60)+")");
+
+ var yAxisGraphic = graphSVG.append("g")
+                     .call(yAxis)
+                     .attr("transform", function(){
+                       return "translate(" + margins.left + "," + (margins.top) + ")";
+                       });
+
+  var xLabel = graphSVG.append("text")
+                        .text("Day")
+                        .attr("x", function(){return graphWidth/2 + margins.left - 20})
+                        .attr("y", function(){return graphHeight + margins.top + 70})
+                        .attr("font-size", 25)
+
+  var title = graphSVG.append("text")
+                        .text("Homeowrk Grades: "+ name)
+                        .attr("x", function(){return graphWidth/2 + margins.left - 150})
+                        .attr("y", function(){return margins.top -10})
+                        .attr("font-size", 25)
+
+    var yLabel = graphSVG.append("text")
+                          .text("Score(%)")
+                          .attr("x", function(){return margins.left - 75})
+                          .attr("y", function(){return margins.top - 10})
+                          .attr("font-size", 20)
+};
+
+//// Line Chart  ////
 var drawLineChart = function(dataSet, svgSelector, index, screen, margins, name)
 {
   console.log("start")
   //
   var quizData = dataSet[index].quizes
-  console.log("QuizData", quizData)
-  console.log(quizData.length)
+  //console.log("QuizData", quizData)
+  //console.log(quizData.length)
   var graphWidth  = screen.width - margins.left - margins.right;
   var graphHeight = screen.height - margins.top - margins.bottom;
   var borderWidth = 1;
@@ -147,8 +246,8 @@ var drawAreaChart = function(dataSet, svgSelector, index, screen, margins, name)
   console.log("start")
   //
   var quizData = dataSet[index].quizes
-  console.log("QuizData", quizData)
-  console.log(quizData.length)
+  //console.log("QuizData", quizData)
+  //console.log(quizData.length)
 
   var graphWidth  = screen.width - margins.left - margins.right;
   var graphHeight = screen.height - margins.top - margins.bottom;
